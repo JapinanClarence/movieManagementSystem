7cbb6171263eec0e8e3da002f86b62bb5c5aa111
JapinanClarence <japinanclarence@gmail.com>
2023-03-02 21:04:02 +0800

    initialize project

---
+++ src/moviemanagementsystem/MovieManagementSystem.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package moviemanagementsystem;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author kaarlosasiang
+ */
+public class MovieManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainFrame mf = new MainFrame();
+
+        mf.setVisible(true);
+
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
